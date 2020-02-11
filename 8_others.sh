d8c619dad3e3e19276ec75ce3f66ea999cde8d2a
vlabsc <vlabs.c@gmail.com>
2020-02-11 11:00:34 +0530

    Delete 8_others.sh

--- 8_others.sh
+++
@@ -1,6 +0,0 @@
-echo " -------- other tools ------------------------------------ "
-sudo pacman -S p7zip p7zip-plugins unrar tar rsync
-sudo pacman -S firefox gedit chromium pluma conky
-
-ln -s /usr/bin/clear /usr/bin/cls
-echo "completed "
