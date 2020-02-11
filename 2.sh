6c4ed4e946458ed884dfeef1e75f07a2cc9bd169
vlabsc <vlabs.c@gmail.com>
2020-02-11 12:58:10 +0530

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -1,6 +1,6 @@
 echo " -------- package ------------------------------------"
 pacman -Syy
-pacman -S reflector
+pacman -S --noconfirm reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 pacstrap /mnt/ base base-devel linux linux-firmware vi sudo vim nano dhcpcd iproute2 man-db man-pages texinfo wget dos2unix
