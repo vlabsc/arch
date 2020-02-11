2bae805c933ede31ac62669a109cdb26f1b97ae9
vlabsc <vlabs.c@gmail.com>
2020-02-11 19:48:45 +0530

    Add files via upload

--- 8.sh
+++ 8.sh
@@ -2,7 +2,7 @@ echo " -------- other tools ------------------------------------ "
 ip link
 sudo dhcpcd enp0s3
 sudo pacman -S --noconfirm firefox gedit chromium pluma konsole conky
-sudo pacman -S --noconfirm p7zip p7zip-plugins unrar tar rsync
+sudo pacman -S --noconfirm p7zip unrar tar rsync
 
 echo "8.sh - essential package installation completed. do a reboot."
 #reboot
