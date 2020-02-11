cb80cdb6a7ac37f15d86dac9748fcc6f0ef89789
vlabsc <vlabs.c@gmail.com>
2020-02-11 16:25:48 +0530

    Add files via upload

--- 7.sh
+++ 7.sh
@@ -5,6 +5,6 @@ useradd -m -g users -G wheel,storage,power -s /bin/bash vlabs
 passwd vlabs
 pacman -S --noconfirm sudo
 echo " Find and uncomment the following line: %wheel ALL=(ALL) ALL"
-visudo
+nano /etc/sudoers
 echo "7.sh completed. do a reboot."
 #reboot
