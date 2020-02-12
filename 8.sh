9e5ad695ebf00a94f7408064940a1d36b21fe0cc
vlabsc <vlabs.c@gmail.com>
2020-02-12 18:49:15 +0530

    Add files via upload

--- 8.sh
+++ 8.sh
@@ -4,7 +4,7 @@ echo " -------- other tools ------------------------------------ "
 sudo pacman -S --noconfirm firefox gedit chromium pluma konsole conky
 sudo pacman -S --noconfirm p7zip unrar tar rsync
 
-sudo pacman -S --noconfirm wireshark-qt hping nmap netcat ruby inetutils gnu-netcat
+sudo pacman -S --noconfirm wireshark-qt hping nmap netcat ruby inetutils gnu-netcat bleachbit
 sudo pacman -Syu
 
 echo "8.sh - essential package installation completed. do a reboot."
